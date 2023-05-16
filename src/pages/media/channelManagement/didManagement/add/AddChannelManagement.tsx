4dc287cff327cc38758ca83f2fde62d1188da3c7
ashu85 <ashutosh.patidar@codiotic.com>
2023-05-16 15:51:17 +0530

    format

--- src/pages/media/channelManagement/didManagement/add/AddChannelManagement.tsx
+++ src/pages/media/channelManagement/didManagement/add/AddChannelManagement.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import MediaLayout from 'src/pages/media/MediaLayout'
 
 const AddChannelManagement = () => {
-    return   <MediaLayout>
-    <div>AddChannelManagement</div>
-</MediaLayout>
+    return (
+        <MediaLayout>
+            <div>AddChannelManagement</div>
+        </MediaLayout>
+    )
 }
 
 export default AddChannelManagement
