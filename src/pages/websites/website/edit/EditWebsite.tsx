9ace8b3af21a130dfee70c8fd6da6dd6a2195a79
mayank joshi <mayank.joshi@codiotic.com>
2023-06-03 18:05:23 +0530

    fixes

--- src/pages/websites/website/edit/EditWebsite.tsx
+++ src/pages/websites/website/edit/EditWebsite.tsx
@@ -37,7 +37,7 @@ const EditWebsite = ({ formikProps, apiStatus }: Props) => {
 
                 {/* Page Heading */}
                 <div className="pt-1">
-                    <ATMPageHeading> Add New Website </ATMPageHeading>
+                    <ATMPageHeading>Update Website </ATMPageHeading>
                 </div>
 
                 <div className="grow max-h-full bg-white border bg-1 rounded shadow  bg-form-bg bg-cover bg-no-repeat">
