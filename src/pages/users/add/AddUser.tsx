95db63b7450f708b9b132551cd435972ed1ffa3b
ashu85 <ashutosh.patidar@codiotic.com>
2023-06-13 13:45:54 +0530

    create a info component

--- src/pages/users/add/AddUser.tsx
+++ src/pages/users/add/AddUser.tsx
@@ -65,6 +65,8 @@ const AddUser = ({ formikProps, apiStatus }: Props) => {
                             {/* FirstName */}
                             <ATMTextField
                                 name="firstName"
+                                isInfo
+                                InfoTitle='please Enter full name  please Enter full nameplease Enter full nameplease Enter full nameplease Enter full nameplease Enter full name'
                                 value={values.firstName}
                                 label="First Name"
                                 placeholder="First Name"
