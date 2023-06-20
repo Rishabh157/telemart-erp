e8955c79b9a32570bd25438f1d534fff7ba0378f
Krati Nayak <tiwari.krati474@gmail.com>
2023-06-20 12:04:26 +0530

    delete action poop up correction

--- src/pages/asr/list/ASRListingWrapper.tsx
+++ src/pages/asr/list/ASRListingWrapper.tsx
@@ -161,8 +161,7 @@ const ASRListingWrapper = () => {
                     }}
                 >
                     <>
-                        <div className="absolute top-8 right-0 bg-white border border-gray-200 rounded-md shadow-lg z-10">
-                            <button
+                        <button
                                 onClick={() => {
                                     navigate(`/asr/${currentId}`)
                                 }}
@@ -187,52 +186,10 @@ const ASRListingWrapper = () => {
                             >
                                 Delete
                             </button>
-                        </div>
+                        
                     </>
                 </ActionPopup>
-                // <div className="relative">
-                //     <button
-                //         onClick={(e) => {
-                //             e.stopPropagation()
-                //             setShowDropdown(!showDropdown)
-                //             setCurrentId(row?._id)
-                //         }}
-                //         className="text-slate-600 font-bold  transition-all duration-[600ms] hover:bg-slate-100 p-2 rounded-full"
-                //     >
-                //         {' '}
-                //         <HiDotsHorizontal className="text-xl text-slate-600 font-bold " />{' '}
-                //     </button>
-                //     {showDropdown && currentId === row?._id && (
-                //         <div className="absolute top-8 right-0 bg-white border border-gray-200 rounded-md shadow-lg z-10">
-                //             <button
-                //                 onClick={() => {
-                //                     navigate(`/asr/${currentId}`)
-                //                 }}
-                //                 className="block w-full text-left px-4 py-2 hover:bg-gray-100"
-                //             >
-                //                 Edit
-                //             </button>
-                //             <button
-                //                 onClick={() => {
-                //                     showConfirmationDialog({
-                //                         title: 'Delete ARS',
-                //                         text: 'Do you want to delete',
-                //                         showCancelButton: true,
-                //                         next: (res) => {
-                //                             return res.isConfirmed
-                //                                 ? handleDelete()
-                //                                 : setShowDropdown(false)
-                //                         },
-                //                     })
-                //                 }}
-                //                 className="block w-full text-left px-4 py-2 hover:bg-gray-100"
-                //             >
-                //                 Delete
-                //             </button>
-                //         </div>
-                //     )}
-                // </div>
-            ),
+                ),
             align: 'end',
         },
     ]
