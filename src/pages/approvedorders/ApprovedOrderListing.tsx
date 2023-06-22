33ee3d77c52435f8af10be588e95a59e0b7eddf0
gour.rishabh <rishabh.gour@codiotic.com>
2023-06-22 15:12:14 +0530

    remove unused icon

--- src/pages/approvedorders/ApprovedOrderListing.tsx
+++ src/pages/approvedorders/ApprovedOrderListing.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import { HiDotsHorizontal } from 'react-icons/hi'
 import ATMTable, {
     columnTypes,
 } from 'src/components/UI/atoms/ATMTable/ATMTable'
