9c01373e826bbcfe69e445ac4b2451a898a6860b
Krati Nayak <tiwari.krati474@gmail.com>
2023-06-23 11:19:23 +0530

    comment

--- src/pages/grn/list/GRNListing.tsx
+++ src/pages/grn/list/GRNListing.tsx
@@ -1,5 +1,17 @@
+/// ==============================================
+// Filename:GRNListing.tsx
+// Type: List Component
+// Last Updated: JUNE 22, 2023
+// Project: TELIMART - Front End
+// ==============================================
+
+// |-- Built-in Dependencies --|
 import React, { useState } from 'react'
+
+// |-- External Dependencies --|
 import { useDispatch, useSelector } from 'react-redux'
+
+// |-- Internal Dependencies --|
 import ATMPageHeading from 'src/components/UI/atoms/ATMPageHeading/ATMPageHeading'
 import ATMPagination from 'src/components/UI/atoms/ATMPagination/ATMPagination'
 import ATMTable from 'src/components/UI/atoms/ATMTable/ATMTable'
@@ -10,8 +22,14 @@ import {
     setSearchValue,
     setFilterValue,
 } from 'src/redux/slices/GRNSlice'
+
+
+// |-- Redux --|
 import { AppDispatch, RootState } from 'src/redux/store'
 
+
+
+// |-- Types --|
 type Props = {
     columns: any[]
     rows: any[]
