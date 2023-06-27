4f10d6924e5f076148942f40efa9f065fe2dbbf1
ashu85 <ashutosh.patidar@codiotic.com>
2023-06-27 18:13:09 +0530

    layout design

--- src/pages/configuration/Configuration Screens/item/add/AddItem.tsx
+++ src/pages/configuration/Configuration Screens/item/add/AddItem.tsx
@@ -46,7 +46,7 @@ const AddItem = ({ formikProps }: Props) => {
     }
     return (
         <div className=" h-[calc(100vh-55px)] overflow-auto">
-        <div className="p-4 flex flex-col gap-2  ">
+            <div className="p-4 flex flex-col gap-2  ">
                 {/* Breadcrumbs */}
                 <div className="">
                     <ATMBreadCrumbs breadcrumbs={breadcrumbs} />
