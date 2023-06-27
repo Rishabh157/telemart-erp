075cd7fb82e676f91e88044e0b0b76ab107f5247
ashu85 <ashutosh.patidar@codiotic.com>
2023-06-27 11:12:25 +0530

    laout-design

--- src/components/UI/Header/Header.tsx
+++ src/components/UI/Header/Header.tsx
@@ -77,7 +77,7 @@ const Header = () => {
     }
 
     return (
-        <div className={`rid grid-cols-2 w-full h-full shadow-lg border `}>
+        <div className={`rid grid-cols-2 w-full h-full shadow-lg borde bg-white`}>
             {/* Right Section */}
             <div className="flex gap-4 col-start-2 justify-end items-center px-4 ">
                 <div>
--- src/pages/userAccess/UserAcess.tsx
+++ src/pages/userAccess/UserAcess.tsx
@@ -75,7 +75,7 @@ const UserAcess = ({
     // const id = open ? 'simple-popover' : undefined
     const { modules } = modulesData
     return (
-        <div className="">
+        <div className="h-[calc(100vh-55px)] bg-white">
             <div className="p-4 flex flex-col gap-2  ">
                 {/* Breadcrumbs */}
                 <div className="">
