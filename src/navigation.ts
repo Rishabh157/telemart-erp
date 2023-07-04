4894369059fcd78eb5f548e572035d1a54c00604
mayank joshi <mayank.joshi@codiotic.com>
2023-07-04 15:00:08 +0530

    icon fixes

--- src/navigation.ts
+++ src/navigation.ts
@@ -3,7 +3,7 @@ import { FiUsers } from 'react-icons/fi'
 import { TbBuildingWarehouse } from 'react-icons/tb'
 import { HiOutlineTruck } from 'react-icons/hi'
 import { FaRegHandshake } from 'react-icons/fa'
-import { CiBoxes } from 'react-icons/ci'
+import { CiBoxes, CiMemoPad, CiStickyNote } from 'react-icons/ci'
 import { CgNotes } from 'react-icons/cg'
 import {
     MdCallReceived,
@@ -11,7 +11,7 @@ import {
     MdPermMedia,
     MdWeb,
 } from 'react-icons/md'
-import { GrCompliance, GrNotes } from 'react-icons/gr'
+
 import { IoCallOutline } from 'react-icons/io5'
 import { AiOutlineSetting } from 'react-icons/ai'
 import { BiBox, BiPurchaseTagAlt } from 'react-icons/bi'
@@ -69,7 +69,7 @@ export const navigation: NavItemType[] = [
     // },
     {
         label: 'ASR',
-        icon: GrCompliance,
+        icon: CiMemoPad,
         path: '/asr',
     },
     {
@@ -79,7 +79,7 @@ export const navigation: NavItemType[] = [
     },
     {
         label: 'GRN',
-        icon: GrNotes,
+        icon: CiStickyNote,
         path: '/grn',
     },
     {
--- src/pages/media/MediaLayout.tsx
+++ src/pages/media/MediaLayout.tsx
@@ -6,16 +6,15 @@ import { BsPersonHeart } from 'react-icons/bs'
 import { BiChevronsLeft, BiCategory } from 'react-icons/bi'
 import { DiDart } from 'react-icons/di'
 import { FaTape } from 'react-icons/fa'
-import { GrChannel } from 'react-icons/gr'
 import { SiGoogletagmanager } from 'react-icons/si'
-import { MdEmojiEvents, MdViewTimeline } from 'react-icons/md'
+import { MdEmojiEvents, MdMonitor, MdViewTimeline } from 'react-icons/md'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { AiOutlineSetting } from 'react-icons/ai'
 
 const mediaNavigation: NavItemType[] = [
     {
         label: 'Channel Group',
-        icon: GrChannel,
+        icon: MdMonitor,
         path: '/media/channel-group',
     },
     {
