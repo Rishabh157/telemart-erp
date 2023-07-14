2ffcde7d1a251aad6e0cbf8587a3bfd3ec8c057b
ashu85 <ashutosh.patidar@codiotic.com>
2023-07-14 15:41:46 +0530

    user-action

--- src/AuthHOC.tsx
+++ src/AuthHOC.tsx
@@ -1,6 +1,6 @@
+import { CircularProgress } from '@mui/material'
 import React from 'react'
 import { useSelector } from 'react-redux'
-import PageNotFound from './PageNotFound'
 import { RootState } from './redux/store'
 import { isCheckAuthorizedModule } from './userAccess/getAuthorizedModules'
 
@@ -26,7 +26,10 @@ const AuthHOC = ({ component, moduleName = '' }: Props) => {
                 <>{component}</>
             ) : (
                 <>
-                    <PageNotFound />
+                <div className='h-[100vh] w-full flex items-center justify-center bg-white'>
+
+                    <CircularProgress />
+                </div>
                 </>
             )}
         </>
