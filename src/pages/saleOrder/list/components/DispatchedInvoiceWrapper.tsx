0f939f3b9feea1f016ec624e89e54e2e930af628
rishabh.gour@codiotic.com <rishabh.gour@codiotic.com>
2024-04-10 10:26:39 +0530

    initial commit for invoice

--- src/pages/saleOrder/list/components/DispatchedInvoiceWrapper.tsx
+++ src/pages/saleOrder/list/components/DispatchedInvoiceWrapper.tsx
@@ -1,13 +1,15 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { useReactToPrint } from 'react-to-print'
 import SideNavLayout from 'src/components/layouts/SideNavLayout/SideNavLayout'
 import DispatchedInvoiceTemplate from './DispatchedInvoiceTemplate'
 import { useGetSalesOrderByIdQuery } from 'src/services/SalesOrderService'
+import { CircularProgress } from '@mui/material'
 
 const DispatchedInvoiceWrapper = () => {
     const navigate = useNavigate()
     const params = useParams()
+    const [invoiceData, setInvoiceData] = useState([])
     const saleOrderId = params?.id
     const saleOrderInvoiceRef = React.useRef(null)
 
@@ -24,6 +26,7 @@ const DispatchedInvoiceWrapper = () => {
 
     useEffect(() => {
         if (!isLoading && !isFetching) {
+            setInvoiceData(data?.data?.[0])
             console.log('data', data)
         }
     }, [isLoading, isFetching, data])
@@ -31,7 +34,15 @@ const DispatchedInvoiceWrapper = () => {
     return (
         <SideNavLayout>
             <div>
-                <DispatchedInvoiceTemplate ref={saleOrderInvoiceRef} />
+                {(isLoading || isFetching) && (
+                    <div className="absolute w-[100%] h-[100%] flex justify-center items-center z-10 bg-slate-100 opacity-50">
+                        <CircularProgress />
+                    </div>
+                )}
+                <DispatchedInvoiceTemplate
+                    ref={saleOrderInvoiceRef}
+                    saleOrderData={invoiceData}
+                />
                 <div className="flex justify-end gap-x-4 m-4">
                     <button
                         className="bg-slate-300 px-2 py-1 text-black rounded"
