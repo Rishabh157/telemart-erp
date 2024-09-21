8d4520488aad7aac076fef071b8a305bee21aa9d
Rishabh157 <rishabhgour157@gmail.com>
2024-09-21 16:19:17 +0530

    add the admin dashboard reports

--- src/hooks/useGetLocalStorage.ts
+++ src/hooks/useGetLocalStorage.ts
@@ -1,4 +1,24 @@
-export const useGetLocalStorage = () => {
+type LocalStorageDataType = {
+    authToken: string | null
+    refreshToken: string | null
+    deviceId: string | null
+    userData: {
+        branchId: string
+        companyId: string
+        email: string
+        firstName: string
+        fullName: string
+        lastName: string
+        mobile: string
+        role: string
+        userDepartment: string
+        userId: string
+        userName: string
+        userRole: string
+    }
+}
+
+export const useGetLocalStorage = (): LocalStorageDataType => {
     const authToken = localStorage.getItem('authToken')
     const refreshToken = localStorage.getItem('refreshToken')
     const deviceId = localStorage.getItem('device-id')
