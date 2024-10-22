de94d0ce5009d02926222a697bdd76a430d5d41e
Rishabh157 <rishabhgour157@gmail.com>
2024-10-22 18:19:55 +0530

    change the customer create complain page

--- src/pages/CustomerComplain/components/ComplaintListing/ComplaintListing.tsx
+++ src/pages/CustomerComplain/components/ComplaintListing/ComplaintListing.tsx
@@ -61,40 +61,40 @@ const ComplaintListing = ({ rows }: Props) => {
             <table className="border border-gray-400 w-full">
                 <thead>
                     <tr className="bg-#cdddf2">
-                        <th className="border border-gray-400 py-2 px-4 text-sm text-center text-[#406698] font-semibold">
+                        <th className="border border-gray-400 py-1 px-2 bg-[#295F98] text-xs font-bold text-center text-white">
                             Action
                         </th>
-                        <th className="border border-gray-400 py-2 px-4 text-sm text-center text-[#406698] font-semibold">
+                        <th className="border border-gray-400 py-1 px-2 bg-[#295F98] text-xs font-bold text-center text-white">
                             No.
                         </th>
-                        <th className="border border-gray-400 py-2 px-4 text-sm text-center text-[#406698] font-semibold">
+                        <th className="border border-gray-400 py-1 px-2 bg-[#295F98] text-xs font-bold text-center text-white">
                             Date
                         </th>
-                        <th className="border border-gray-400 py-2 px-4 text-sm text-center text-[#406698] font-semibold">
+                        <th className="border border-gray-400 py-1 px-2 bg-[#295F98] text-xs font-bold text-center text-white">
                             Order No.
                         </th>
-                        <th className="border border-gray-400 py-2 px-4 text-sm text-center text-[#406698] font-semibold">
+                        <th className="border border-gray-400 py-1 px-2 bg-[#295F98] text-xs font-bold text-center text-white">
                             Call Type
                         </th>
-                        <th className="border border-gray-400 py-2 px-4 text-sm text-center text-[#406698] font-semibold">
+                        <th className="border border-gray-400 py-1 px-2 bg-[#295F98] text-xs font-bold text-center text-white">
                             Issue Category (IC1:IC2:IC3)
                         </th>
-                        <th className="border border-gray-400 py-2 px-4 text-sm text-center text-[#406698] font-semibold">
+                        <th className="border border-gray-400 py-1 px-2 bg-[#295F98] text-xs font-bold text-center text-white">
                             Status (Return Type)
                         </th>
-                        <th className="border border-gray-400 py-2 px-4 text-sm text-center text-[#406698] font-semibold">
+                        <th className="border border-gray-400 py-1 px-2 bg-[#295F98] text-xs font-bold text-center text-white">
                             Stage
                         </th>
-                        <th className="border border-gray-400 py-2 px-4 text-sm text-center text-[#406698] font-semibold">
+                        <th className="border border-gray-400 py-1 px-2 bg-[#295F98] text-xs font-bold text-center text-white">
                             Last Remark
                         </th>
-                        <th className="border border-gray-400 py-2 px-4 text-sm text-center text-[#406698] font-semibold">
+                        <th className="border border-gray-400 py-1 px-2 bg-[#295F98] text-xs font-bold text-center text-white">
                             Last Updated By
                         </th>
-                        <th className="border border-gray-400 py-2 px-4 text-sm text-center text-[#406698] font-semibold">
+                        <th className="border border-gray-400 py-1 px-2 bg-[#295F98] text-xs font-bold text-center text-white">
                             Last Updated Date
                         </th>
-                        <th className="border border-gray-400 py-2 px-4 text-sm text-center text-[#406698] font-semibold">
+                        <th className="border border-gray-400 py-1 px-2 bg-[#295F98] text-xs font-bold text-center text-white">
                             Total Calls
                         </th>
                     </tr>
@@ -136,11 +136,11 @@ const ComplaintListing = ({ rows }: Props) => {
                                     )}
                                 </td>
 
-                                <td className="border border-gray-400 py-2 px-4 text-sm text-center text-[#406698] font-semibold">
+                                <td className="border border-gray-400 py-2 px-4 text-xs text-center text-slate-600 font-bold">
                                     {ele?.complaintNumber}
                                 </td>
 
-                                <td className="border border-gray-400 py-2 px-4 text-sm text-center text-[#406698] font-semibold">
+                                <td className="border border-gray-400 py-2 px-4 text-xs text-center text-slate-600 font-bold">
                                     <div className="flex flex-col">
                                         <span>
                                             {moment(ele?.createdAt).format(
@@ -155,13 +155,13 @@ const ComplaintListing = ({ rows }: Props) => {
                                         </span>
                                     </div>
                                 </td>
-                                <td className="border border-gray-400 py-2 px-4 text-sm text-center text-[#406698] font-semibold">
+                                <td className="border border-gray-400 py-2 px-4 text-xs text-center text-slate-600 font-bold">
                                     {ele?.orderNumber}
                                 </td>
-                                <td className="border border-gray-400 py-2 px-4 text-sm text-center text-[#406698] font-semibold">
+                                <td className="border border-gray-400 py-2 px-4 text-xs text-center text-slate-600 font-bold">
                                     {ele?.callType}
                                 </td>
-                                <td className="border border-gray-400 py-2 px-4 text-sm text-center text-[#406698] font-semibold">
+                                <td className="border border-gray-400 py-2 px-4 text-xs text-center text-slate-600 font-bold">
                                     {ele?.initialCallOneLabel}
                                     {':'}
                                     <br />
@@ -170,19 +170,19 @@ const ComplaintListing = ({ rows }: Props) => {
                                     <br />
                                     {ele?.initialCallThreeLabel}
                                 </td>
-                                <td className="border border-gray-400 py-2 px-4 text-sm text-center text-[#406698] font-semibold">
+                                <td className="border border-gray-400 py-2 px-4 text-xs text-center text-slate-600 font-bold">
                                     {ele?.status}
                                 </td>
-                                <td className="border border-gray-400 py-2 px-4 text-sm text-center text-[#406698] font-semibold">
+                                <td className="border border-gray-400 py-2 px-4 text-xs text-center text-slate-600 font-bold">
                                     -
                                 </td>
-                                <td className="border border-gray-400 py-2 px-4 text-sm text-center text-[#406698] font-semibold">
+                                <td className="border border-gray-400 py-2 px-4 text-xs text-center text-slate-600 font-bold">
                                     {ele?.remark}
                                 </td>
-                                <td className="border border-gray-400 py-2 px-4 text-sm text-center text-[#406698] font-semibold">
+                                <td className="border border-gray-400 py-2 px-4 text-xs text-center text-slate-600 font-bold">
                                     -
                                 </td>
-                                <td className="border border-gray-400 py-2 px-4 text-sm text-center text-[#406698] font-semibold">
+                                <td className="border border-gray-400 py-2 px-4 text-xs text-center text-slate-600 font-bold">
                                     {moment(ele?.updatedAt).format(
                                         'DD-MM-YYYY'
                                     )}
@@ -190,7 +190,7 @@ const ComplaintListing = ({ rows }: Props) => {
                                         'hh:mm:ss A'
                                     )}
                                 </td>
-                                <td className="border border-gray-400 py-2 px-4 text-sm text-center text-[#406698] font-semibold">
+                                <td className="border border-gray-400 py-2 px-4 text-xs text-center text-slate-600 font-bold">
                                     -
                                 </td>
                             </tr>
