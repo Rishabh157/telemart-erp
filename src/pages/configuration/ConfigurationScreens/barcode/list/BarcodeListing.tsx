3bd3bdaa6f8c12b6396b7a49d1f488710cc499aa
Rishabh157 <rishabhgour157@gmail.com>
2024-11-15 21:19:37 +0530

    Add the Barcode

--- src/pages/configuration/ConfigurationScreens/barcode/list/BarcodeListing.tsx
+++ src/pages/configuration/ConfigurationScreens/barcode/list/BarcodeListing.tsx
@@ -1,50 +1,52 @@
 // |-- External Dependencies --|
+import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
 // |-- Internal Dependencies --|
 import ATMPageHeading from 'src/components/UI/atoms/ATMPageHeading/ATMPageHeading'
-import ATMPagination from 'src/components/UI/atoms/ATMPagination/ATMPagination'
 import ATMTableHeader from 'src/components/UI/atoms/ATMTableHeader/ATMTableHeader'
-import {
-    setRowsPerPage,
-    setPage,
-    setSearchValue,
-} from 'src/redux/slices/ListingPaginationSlice'
+import { setSearchValue } from 'src/redux/slices/ListingPaginationSlice'
 import { BarcodeListResponseType } from 'src/models'
-import BarcodeDetailsCard from './components/BarcodeDetailsCard/BarcodeDetailsCard'
 
 // |-- Redux --|
 import { AppDispatch, RootState } from 'src/redux/store'
 import { UserModuleNameTypes } from 'src/utils/mediaJson/userAccess'
 import { isAuthorized } from 'src/utils/authorization'
+import { FaRegCopy } from 'react-icons/fa'
+import { LuCopyCheck } from 'react-icons/lu'
+import moment from 'moment'
 
 // |-- Types --|
 type Props = {
-    rows: any[]
-    selectedBarcodes: BarcodeListResponseType[]
-    onBarcodeSelect: (
-        e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
-        barcode: BarcodeListResponseType,
-        isBarcodeSeleted: boolean
-    ) => void
-    onBarcodeClick: (barcode: BarcodeListResponseType) => void
+    items: BarcodeListResponseType
 }
 
-const BarcodeListing = ({
-    rows,
-    selectedBarcodes,
-    onBarcodeSelect,
-    onBarcodeClick,
-}: Props) => {
+const BarcodeListing = ({ items }: Props) => {
+
+    
+    console.log('items: ', items);
+
+    const [copiedBarcode, setCopiedBarcode] = useState<string | null>(null);
+
+    const barcodeState: any = useSelector((state: RootState) => state.listingPagination)
+    const { searchValue } = barcodeState
+
     // Hooks
     const navigate = useNavigate()
     const dispatch = useDispatch<AppDispatch>()
-    const barcodeState: any = useSelector(
-        (state: RootState) => state.listingPagination
-    )
 
-    const { page, rowsPerPage, totalItems, searchValue } = barcodeState
+    // copy for barcode
+    const handleCopyText = (barcodeNumber: string) => {
+        navigator.clipboard.writeText(barcodeNumber)
+            .then(() => {
+                setCopiedBarcode(barcodeNumber);
+                setTimeout(() => setCopiedBarcode(null), 2000); // Tooltip disappears after 2 seconds
+            })
+            .catch((err) => {
+                console.error('Failed to copy text: ', err);
+            });
+    };
 
     return (
         <div className="px-4 h-[calc(100%-55px)] flex flex-col gap-3">
@@ -65,31 +67,138 @@ const BarcodeListing = ({
                 {/* Header */}
                 <ATMTableHeader
                     searchValue={searchValue}
-                    page={page}
-                    rowCount={totalItems}
-                    rowsPerPage={rowsPerPage}
-                    rows={rows}
-                    onRowsPerPageChange={(newValue) =>
-                        dispatch(setRowsPerPage(newValue))
-                    }
-                    // isFilter
+                    rows={[]}
+                    isHiddenPagination
                     onSearch={(newValue) => dispatch(setSearchValue(newValue))}
                 />
 
-                {/* Barcode Detail Cards */}
-                <div className="grow overflow-auto">
-                    <BarcodeDetailsCard barcodeList={rows} />
-                </div>
+                {/* <div className="grow overflow-auto"> */}
+                {items && (<div className={`flex text-xs flex-col gap-2 shadow rounded-lg border-[1.5px] p-2 border-slate-200`}>
+                    <div className="flex justify-end">
+                        <div>
+                            {!items?.isUsed && (
+                                <span className="text-white bg-red-500 px-2 text-[11px] rounded-full inline-flex items-center py-[1px] font-medium">
+                                    Used
+                                </span>
+                            )}
+                        </div>
+                    </div>
+
+                    <div className="flex justify-between">
+                        <div className=" text-slate-500 w-28">
+                            Vendor :-
+                        </div>
+                        <div className="text-primary-main font-medium w-full truncate grow flex items-end">
+                            <span className='w-full truncate'
+                                title={items?.vendorLabel}>
+                                {items?.vendorLabel}
+                            </span>
+                        </div>
+                    </div>
+                    <div className="flex justify-between">
+                        <div className=" text-slate-500 mr-6 w-34">
+                            Product group :-
+                        </div>
+                        <div className="text-primary-main font-medium grow flex items-end">
+                            {items?.productGroupLabel}{' '}
+                        </div>
+                    </div>
+                    <div className="flex justify-between">
+                        <div className=" text-slate-500 mr-5 w-34">
+                            Upper items :-
+                        </div>
+                        <div className="text-primary-main font-medium grow flex items-end">
+                            {items?.upperBarcodeNumber}
+                        </div>
+                    </div>
+                    <div className="flex justify-between">
+                        <div className=" text-slate-500 mr-10 w-34">
+                            Lot Number :-
+                        </div>
+                        <div className="text-primary-main font-medium grow flex items-end">
+                            {items?.lotNumber}
+                        </div>
+                    </div>
+
+                    <div className="flex justify-between items-center">
+                        <div className=" text-slate-500 mr-14 w-34">
+                            Barocde :-
+                        </div>
+                        <div className="relative group text-primary-main font-medium grow flex items-end">
+
+                            <span className='bg-slate-300 font-semibold px-2 py-1 rounded'>{items?.barcodeNumber}</span>
+
+                            <div className="relative flex left-3 -top-1 justify-end items-center opacity-0 invisible transition-all group-hover:opacity-100 group-hover:visible">
+                                {copiedBarcode !== items?.barcodeNumber ?
+                                    <FaRegCopy
+                                        title="Copy Barcode"
+                                        size={14}
+                                        className="cursor-pointer text-primary-main hover:text-blue-700 transition"
+                                        onClick={() => handleCopyText(items?.barcodeNumber)}
+                                    /> : <LuCopyCheck
+                                        size={14}
+                                        className="cursor-pointer text-blue-500 hover:text-blue-700 transition"
+                                    />
+                                }
+                                <span className='text-xs pl-1'>
+                                    {copiedBarcode === items?.barcodeNumber ? 'Copied!' : 'Copy'}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex justify-between">
+                        <div className=" text-slate-500 mr-7 w-34">
+                            Created Date :-
+                        </div>
+                        <div className="text-primary-main font-medium grow flex items-end">
+                            {moment(items?.createdAt).format('D/MM/YYYY hh:mm A')}
+                        </div>
+                    </div>
+
+                    <div className="flex justify-between">
+                        <div className=" text-slate-500 mr-8 w-34">
+                            Expired Date :-
+                        </div>
+                        <div className="text-primary-main font-medium grow flex items-end">
+                            {items?.expiryDate !== null ? (
+                                <>
+                                    {moment(items?.expiryDate).format('D/MM/YYYY')}
+                                    {moment(items?.expiryDate).isBefore(moment()) && (
+                                        <span className="ml-2 text-red-500 text-[10px] font-semibold">
+                                            (Expired)
+                                        </span>
+                                    )}
+                                </>
+                            ) : (
+                                'NA'
+                            )}
+                        </div>
+                    </div>
+
+                    <div className="flex justify-between items-center">
+                        <div className=" text-slate-500 mr-6 w-34">
+                            Current Status :-
+                        </div>
+                        <div className='grow flex items-end'>
+                            <span className='bg-slate-300 text-primary-main font-semibold px-2 py-1 rounded'>
+                                {items?.status?.replaceAll('_', ' ') || '-'}
+                            </span>
+                        </div>
+                    </div>
+                </div>)
+                }
+                {/* </div> */}
 
                 {/* Pagination */}
                 <div className="h-[60px] flex items-center justify-end border-t border-slate-300">
-                    <ATMPagination
+                    {/* <ATMPagination
                         page={page}
                         rowCount={totalItems}
                         rows={rows}
                         rowsPerPage={rowsPerPage}
                         onPageChange={(newPage) => dispatch(setPage(newPage))}
-                    />
+                    /> */}
                 </div>
             </div>
         </div>
--- src/pages/configuration/ConfigurationScreens/barcode/list/BarcodeListingWrapper.tsx
+++ src/pages/configuration/ConfigurationScreens/barcode/list/BarcodeListingWrapper.tsx
@@ -8,15 +8,11 @@ import { MdOutbond } from 'react-icons/md'
 import { BiFilter } from 'react-icons/bi'
 
 // |-- Internal Dependencies --|
-import {
-    BarcodeListResponseType,
-    ProductBarcodeGroupResponse,
-} from 'src/models'
-
+import { ProductBarcodeGroupResponse } from 'src/models'
 import BarcodeListing from './BarcodeListing'
 import {
-    useGetBarcodeQuery,
     useGetProductGroupBarcodeQuery,
+    useGetBarcodeByBarcodeNumberQuery
 } from 'src/services/BarcodeService'
 import {
     setIsTableLoading as pgSetIsTableLoading,
@@ -30,9 +26,9 @@ import ProductGroupListing from './components/BarcodeGroup/ProductGroupBarcodeLi
 import { AppDispatch, RootState } from 'src/redux/store'
 import { isAuthorized } from 'src/utils/authorization'
 import { UserModuleNameTypes } from 'src/utils/mediaJson/userAccess'
-import useGetCustomListingData from 'src/hooks/useGetCustomListingData'
 import useUnmountCleanup from 'src/hooks/useUnmountCleanup'
 import ReprintOuterBoxBarcode from './components/ReprintOuterBoxBarcode/ReprintOuterBoxBarcode'
+import useGetDataByIdCustomQuery from 'src/hooks/useGetDataByIdCustomQuery'
 
 // |-- Types --|
 export type Tabs = {
@@ -50,43 +46,26 @@ export type barcodecardType = {
 }
 
 const BarcodeListingWrapper = () => {
+
     useUnmountCleanup()
     const barcodeState: any = useSelector(
         (state: RootState) => state.listingPagination
     )
     const [groupBarcode, setGroupBarcode] = useState('')
     const [activeTabIndex, setActiveTabIndex] = useState(0)
-    const { page, rowsPerPage, searchValue } = barcodeState
+    const [selectedProductGroup, setSelectedProductGroup] = React.useState<ProductBarcodeGroupResponse[]>([])
+    const { searchValue } = barcodeState
     const dispatch = useDispatch<AppDispatch>()
-    const { userData } = useSelector((state: RootState) => state?.auth)
 
-    const { items } = useGetCustomListingData({
-        useEndPointHook: useGetBarcodeQuery({
-            limit: rowsPerPage,
-            searchValue: searchValue,
-            params: ['barcodeNumber', 'productGroupLabel'],
-            page: page,
-            filterBy: [
-                {
-                    fieldName: 'barcodeGroupNumber',
-                    value: [groupBarcode],
-                },
-                {
-                    fieldName: 'companyId',
-                    value: userData?.companyId,
-                },
-            ],
-            dateFilter: {},
+    // const { userData } = useSelector((state: RootState) => state?.auth)
 
-            orderBy: 'createdAt',
-            orderByValue: -1,
-            isPaginationRequired: true,
+    const { items } = useGetDataByIdCustomQuery({
+        useEndPointHook: useGetBarcodeByBarcodeNumberQuery(searchValue, {
+            skip: searchValue.length <= 8 // Skip if length is 14 or less
         }),
     })
 
-    const productGroupState: any = useSelector(
-        (state: RootState) => state.productGroupBarcode
-    )
+    const productGroupState: any = useSelector((state: RootState) => state.productGroupBarcode)
 
     const {
         page: pgPage,
@@ -128,14 +107,6 @@ const BarcodeListingWrapper = () => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [pgIsLoading, pgIsFetching, pgData])
 
-    const [selectedBarcodes, setSelectedBarcodes] = React.useState<
-        BarcodeListResponseType[]
-    >([])
-
-    const [selectedProductGroup, setSelectedProductGroup] = React.useState<
-        ProductBarcodeGroupResponse[]
-    >([])
-
     const onProductGroupSelect = (
         e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
         barcode: ProductBarcodeGroupResponse,
@@ -153,25 +124,6 @@ const BarcodeListingWrapper = () => {
         }
 
         setSelectedProductGroup(newValue)
-    }
-    // Handle Barcode Select
-    const onBarcodeSelect = (
-        e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
-        barcode: BarcodeListResponseType,
-        isBarcodeSeleted: boolean
-    ) => {
-        e.stopPropagation()
-        let newValue = []
-        if (isBarcodeSeleted) {
-            newValue = selectedBarcodes.filter(
-                (seleted: BarcodeListResponseType) =>
-                    seleted._id !== barcode._id
-            )
-        } else {
-            newValue = [...selectedBarcodes, barcode]
-        }
-
-        setSelectedBarcodes(newValue)
     }
 
     const tabs: Tabs[] = [
@@ -200,7 +152,7 @@ const BarcodeListingWrapper = () => {
             name: UserModuleNameTypes.ACTION_REPRINT_OUTERBOX_BARCODE,
         },
     ]
-    
+
     const allowedTabs = tabs
         ?.filter((nav) => {
             return isAuthorized(nav?.name as keyof typeof UserModuleNameTypes)
@@ -219,11 +171,10 @@ const BarcodeListingWrapper = () => {
                                 setActiveTabIndex(index)
                             }}
                             key={tabIndex}
-                            className={`flex items-center gap-2 px-4 h-[calc(100%-14px)] rounded transition-all duration-500 ${
-                                activeTabIndex === index
-                                    ? 'bg-slate-100 text-primary-main '
-                                    : 'text-slate-500'
-                            }`}
+                            className={`flex items-center gap-2 px-4 h-[calc(100%-14px)] rounded transition-all duration-500 ${activeTabIndex === index
+                                ? 'bg-slate-100 text-primary-main '
+                                : 'text-slate-500'
+                                }`}
                         >
                             <div>
                                 {' '}
@@ -233,7 +184,7 @@ const BarcodeListingWrapper = () => {
                             {index === 0 && groupBarcode.length ? (
                                 <button
                                     onClick={() => setGroupBarcode('')}
-                                    className="bg-blue-400   flex items-center rounded border"
+                                    className="bg-blue-400 flex items-center rounded border"
                                 >
                                     <BiFilter
                                         color="white"
@@ -247,12 +198,7 @@ const BarcodeListingWrapper = () => {
             </div>
 
             {activeTabIndex === 0 ? (
-                <BarcodeListing
-                    rows={items}
-                    selectedBarcodes={selectedBarcodes}
-                    onBarcodeSelect={onBarcodeSelect}
-                    onBarcodeClick={() => {}}
-                />
+                <BarcodeListing items={items as any} />
             ) : activeTabIndex === 1 ? (
                 <CartonBoxBarcodeListing />
             ) : activeTabIndex === 2 ? (
